37b9890db552530bbc72414353a54a1d6b48a817
Hitesh Tarwani <hiteshtarwani4@gmail.com>
2023-05-16 18:22:31 +0530

    Ui

---
+++ components/Featured.jsx
@@ -0,0 +1,59 @@
+import styles from "../styles/Featured.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+const Featured = () => {
+  const [index, setIndex] = useState(0);
+  const images = [
+    "/img/featured.jpg",
+    "/img/featured2.jpg",
+    "/img/featured3.jpg",
+  ];
+
+  const handleArrow = (direction) => {
+    if (direction === "l") {
+      setIndex(index !== 0 ? index - 1 : 2);
+    }
+    if (direction === "r") {
+      setIndex(index !== 2 ? index + 1 : 0);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
+        <Image src="/img/arrowl.png" alt="" layout="fill" objectfit="contain" />
+      </div>
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${-100 * index}vw)` }}
+      >
+        {images.map((img, index) => (
+          <div className={styles.imgContainer} key={index}>
+            <img src={img} alt="" layout="fill" objectfit="contain" />
+          </div>
+        ))}
+      </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
+        <Image
+          src="/img/arrowr.png"
+          layout="fill"
+          alt=""
+          objectFit="cover"
+          width={"100vw"}
+          height={"100%"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured />
+      <PizzaList />
+    </div>
+  );
+}
